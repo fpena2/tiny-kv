576c9d0b4aec5823be1e224ad720b874a46269bc
fpena2 <f.j.pena@wustl.edu>
2025-04-06 18:19:54 -0500

    connect backend to server requests

--- src/service.rs
+++ src/service.rs
@@ -1,5 +1,5 @@
 use crate::proto::tinykv_server::Tinykv; // proto pakage trait
-use crate::proto::{ActionResult, DeleteRequest, GetRequest, PutRequest};
+use crate::proto::{DeleteRequest, GetRequest, PutRequest, TinykvResult};
 use crate::storange::Storage;
 
 #[derive(Debug, Default)]
@@ -12,25 +12,67 @@ impl Tinykv for Service {
     async fn put(
         &self,
         request: tonic::Request<PutRequest>,
-    ) -> Result<tonic::Response<ActionResult>, tonic::Status> {
-        println!("Got a request: {:?}", request);
-        let reply = ActionResult {
-            result: format!("Hello!"),
-        };
-        Ok(tonic::Response::new(reply))
+    ) -> Result<tonic::Response<TinykvResult>, tonic::Status> {
+        let msg = request.into_inner();
+        let cf = msg.cf;
+        let k = msg.k;
+        let v = msg.v;
+
+        match self.db.put(&cf, &k, &v) {
+            Ok(()) => Ok(tonic::Response::new(TinykvResult {
+                success: true,
+                data: String::from(""),
+                error: String::from(""),
+            })),
+            Err(e) => Ok(tonic::Response::new(TinykvResult {
+                success: false,
+                data: String::from(""),
+                error: e.to_string(),
+            })),
+        }
     }
 
     async fn get(
         &self,
         request: tonic::Request<GetRequest>,
-    ) -> Result<tonic::Response<ActionResult>, tonic::Status> {
-        todo!()
+    ) -> Result<tonic::Response<TinykvResult>, tonic::Status> {
+        let msg = request.into_inner();
+        let cf = msg.cf;
+        let k = msg.k;
+
+        match self.db.get(&cf, &k) {
+            Ok(value) => Ok(tonic::Response::new(TinykvResult {
+                success: true,
+                data: value,
+                error: String::from(""),
+            })),
+            Err(e) => Ok(tonic::Response::new(TinykvResult {
+                success: false,
+                data: String::from(""),
+                error: e.to_string(),
+            })),
+        }
     }
 
     async fn delete(
         &self,
         request: tonic::Request<DeleteRequest>,
-    ) -> Result<tonic::Response<ActionResult>, tonic::Status> {
-        todo!()
+    ) -> Result<tonic::Response<TinykvResult>, tonic::Status> {
+        let msg = request.into_inner();
+        let cf = msg.cf;
+        let k = msg.k;
+
+        match self.db.delete(&cf, &k) {
+            Ok(_value) => Ok(tonic::Response::new(TinykvResult {
+                success: true,
+                data: String::from(""),
+                error: String::from(""),
+            })),
+            Err(e) => Ok(tonic::Response::new(TinykvResult {
+                success: false,
+                data: String::from(""),
+                error: e.to_string(),
+            })),
+        }
     }
 }
